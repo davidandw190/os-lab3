788f77cac49d7a59917e9c8ccabfd12f6a21d8ee
Andrei-David Nan <nandavid190@gmail.com>
2023-10-18 19:30:58 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 1024
+
+
+void move_file(const char *source, const char *dest) {
+    int source_fd, dest_fd;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read, bytes_written;
+
+    source_fd = open(source, O_RDONLY);
+    if (source_fd == -1) {
+        perror("Error opening source file");
+        exit(1);
+    }
+
+    dest_fd = open(dest, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+    if (dest_fd == -1) {
+        perror("Error openning destination file");
+        close(source_fd);
+        exit(1);
+    }
+
+    while ((bytes_read = read(source_fd, buffer, BUFFER_SIZE)) > 0) {
+        bytes_written = write(dest_fd, buffer, bytes_read);
+
+        if (bytes_written == -1) {
+            perror("Error writing to destination file");
+            close(source_fd);
+            close(dest_fd);
+            exit(1);
+        }
+    }
+
+    if (bytes_read == -1) {
+        perror("Error reading from source file");
+    }
+
+    close(source_fd);
+    close(dest_fd);
+
+    // Delete the source file
+    if (unlink(source) == -1) {
+        perror("Error deleting source file");
+    }
+
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        printf("Usage: %s <source_file> <dest_file>\n", argv[0]);
+        exit(1);
+    }
+
+    const char *source_file = argv[1];
+    const char *dest_file = argv[2];
+
+    move_file(source_file, dest_file);
+
+    return 0;
+}
